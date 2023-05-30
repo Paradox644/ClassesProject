7f5a3f6e1e1e82e2326df24bba1ced14bc990794
Paradoxwastaken <132946783+Paradoxwastaken@users.noreply.github.com>
2023-05-31 00:14:07 +0300

    Add files via upload

---
+++ ClassesProject/ClassesProject/ClassesProject.cpp
@@ -0,0 +1,68 @@
+﻿// ClassesProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include"Header.h"
+#include <vector>
+#include <cstdlib>
+/*
+template <class TELEM>
+class TContainer
+{
+protected:
+	TELEM* elem; // массив хранимых элементов
+	int size; // размер массива
+	int count; // к-во реально хранимых элементов
+	const static int sizestep = 10; // шаг наращивания размера массива
+public:
+	TContainer() {
+		size = 10;
+	}
+		
+	TContainer(const TContainer& _cnt);
+	~TContainer();
+	const int& Сount() const { return count; }
+	int& Сount() { return count; }
+	const int& Sizestep() const { return sizestep; }
+	void Add(TELEM _elm); // добавить элемент
+	void Del(TELEM _elm); // удалить элемент по его значению
+	void Del(int index); // удалить элемент по его номеру
+	TELEM& operator[](int index); // индексация по номеру
+		TELEM& operator[](const TELEM& _elm); // индексация по содержанию
+		const TELEM& operator[](int index) const; //индексация по номеру
+		const TELEM& operator[](const TELEM& _elm) const; //индексация по содержанию
+		TContainer& operator=(const TContainer& _cnt); //присваивание контейнеров
+		TContainer operator+(const TContainer& _cnt); // объединение контейнеров
+		TContainer operator*(const TContainer& _cnt); // пересечение контейнеров
+		TContainer operator-(const TContainer& _cnt); // разность контейнеров
+protected:
+	void resize(int dsize = 0); // увеличить длину контейнера
+		int _find(const TELEM& _elm) const; // найти элемент
+};
+*/
+int main()
+{
+	Player protag,protagfriend;
+	Enemy orc;
+	std::vector <PlayableCharacter> vec;
+	vec.push_back(protag);
+	vec.push_back(protagfriend);
+	std::cout << vec.size();
+	vec.pop_back();
+	std::cout << vec.size();
+
+	//std::cout << protag;
+	//protag.print_playercount();
+	//std::cout << protag;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ ClassesProject/ClassesProject/Header.h
@@ -0,0 +1,206 @@
+﻿#pragma once
+#ifndef Header_h
+#define Header_h
+#include<iostream>
+#include<cstdlib>
+//Структура:
+//CreatureEntity
+//	NPC
+//		Enemy 
+//		Vendor 
+//			Blacksmith 
+//	PlayableCharacter
+//		Player
+//		Summon
+static unsigned PlayerCount=0;
+class CreatureEntity
+{
+protected:
+	double HP_max, MP_max,HP_current,MP_current;
+	unsigned gold;
+
+public:
+
+	double get_current_health() {
+		return HP_current;
+	}
+	double get_current_mana() {
+		return MP_current;
+	}
+	void set_health(double _HP) {
+		HP_current = _HP;
+	}
+	void set_mana(double _MP) {
+		MP_current = _MP;
+	}
+	unsigned get_gold() {
+		return gold;
+	}
+	void set_gold(unsigned _gold) {
+		gold = _gold;
+	}
+	virtual void heal(double heal_amount) = 0;
+	virtual void deal_damage(double damage_amt, CreatureEntity& _enemy) = 0;
+	
+};
+class NPC :public CreatureEntity
+{
+protected:
+	
+public:
+	
+};
+class Enemy : public NPC
+{
+protected:
+public:
+	Enemy() {
+		HP_max = 100 + 100 * PlayerCount * 0.5;
+		MP_max = 100 + 100 * PlayerCount * 0.25;
+		set_health(HP_max);
+		set_mana(MP_max);
+
+	}
+	Enemy(double _base_hp, double _base_mp) {
+		HP_current = HP_max = _base_hp+_base_hp*PlayerCount*0.5;
+		MP_current = MP_max = _base_mp + _base_mp * PlayerCount * 0.5;
+	}
+
+	void heal(double heal_amount) {
+		set_health(get_current_health() + heal_amount * 0.9); //лечение, получаемое противником, немного уменьшено
+	}
+	void deal_damage(double damage_amt, CreatureEntity& _enemy) {
+		_enemy.set_health(get_current_health() - damage_amt);
+	}
+	friend std::ostream& operator<<(std::ostream&, Enemy&);
+};
+std::ostream& operator<<(std::ostream& out, Enemy& e) {
+	out << "\nHP: " << e.HP_current << "/" << e.HP_max;
+	out << "\nMP: " << e.MP_current << "/" << e.MP_max;
+	return out;
+}
+class Vendor :public NPC
+{
+protected:
+public:
+	Vendor() {
+		set_health(9999);
+		set_mana(0);
+		set_gold(1000);
+	}
+
+};
+class Blacksmith :public Vendor
+{
+protected:
+	unsigned scrap_metal;
+public:
+	unsigned get_scrap() {
+		return scrap_metal;
+	}
+	void set_scrap_metal(unsigned _scrap) {
+		scrap_metal = _scrap;
+	}
+};
+class PlayableCharacter : public CreatureEntity
+{
+
+};
+class Player : public PlayableCharacter
+{
+protected:
+
+	unsigned player_level,skill_points,healing_spell_level,fireball_level,mana_steal_level,hp_gain,mp_gain;
+public:
+
+	static void print_playercount() {
+		std::cout << "There are " << PlayerCount << " players in the game.";
+	}
+	Player() {
+		PlayerCount++;
+		HP_max = 100;
+		MP_max = 100;
+		set_health(HP_max);
+		set_mana(MP_max);
+		player_level = skill_points = 1;
+		healing_spell_level = fireball_level = mana_steal_level = 0;
+		hp_gain = mp_gain = 20;
+	}
+	void heal(double heal_amount) {
+		set_health(get_current_health() + heal_amount * 1.5); // лечение, получаемое игроком, увеличено
+	}
+	void Purification() {
+		unsigned mana_required = 20 * healing_spell_level;
+		if (get_current_mana() > mana_required)
+		{
+			heal(30 * healing_spell_level);
+			set_mana(get_current_mana() - mana_required);
+		}
+		else std::cout << "\nNot enough mana!\n";
+	}
+	void deal_damage(double damage_amt,CreatureEntity& _enemy) {
+		_enemy.set_health(_enemy.get_current_health() - damage_amt);
+	}
+	void Fireball(CreatureEntity& _enemy) {
+		unsigned mana_required = 30 * fireball_level;
+		if (get_current_mana() > mana_required) {
+			deal_damage(40 * fireball_level, _enemy);
+			set_mana(get_current_mana() - mana_required);
+		}
+		else std::cout << "\nNot enough mana!\n";
+
+	}
+	void Mana_steal(CreatureEntity& _enemy) {
+		unsigned mana_stolen = 60 * mana_steal_level;
+		_enemy.set_mana(get_current_mana() - mana_stolen);
+		set_mana(get_current_mana() + mana_stolen);
+
+	}
+	void LevelUP() {
+		skill_points++;
+		player_level++;
+		set_health(HP_max + hp_gain);
+		set_mana(MP_max + mp_gain);
+		std::cout << "\nYou have leveled up! You have been healed to full and gained " << hp_gain << "max health and " << mp_gain << "max mana. You have " << skill_points << "unspent skill points.\n";
+
+	}
+	void AbilityLevelUp() {
+		std::cout << "\nChoose an ability to level up:\n 1.Purification\n 2.Fireball\n 3. Mana steal\n";
+		int abilitycode;
+		do
+			std::cin >> abilitycode;
+		while ((abilitycode < 1) || (abilitycode > 3));
+		switch (abilitycode)
+		{
+			case 1:
+				healing_spell_level++;
+				break;
+			case 2:
+				fireball_level++;
+				break;
+			case 3:
+				mana_steal_level++;
+				break;
+		}
+		skill_points--;
+	}
+	friend std::ostream& operator<<(std::ostream&, const Player&);
+};
+
+std::ostream& operator<<(std::ostream& out, const Player& p) {
+	out << "\nLevel: " << p.player_level;
+	out << "\nHP: " << p.HP_current << "/" << p.HP_max;
+	out << "\nMana: " << p.MP_current << "/" << p.MP_max;
+	out << "\nUnspent skill points: " << p.skill_points;
+	out << "\nAbility levels: \nPurification: " << p.healing_spell_level << "\nFireball: " << p.fireball_level << "\nMana steal: " << p.mana_steal_level;
+	return out;
+}
+class Summon : PlayableCharacter
+{
+private:
+public:
+	void heal(double heal_amount) {
+		set_health(get_current_health() + heal_amount * 0.75); //лечение, получаемое призываемым существом, уменьшено
+	}
+};
+#endif // !Header_h
